34c7042197bd032783ee8652c620aa4b039c0330
Meliih <melihkahraman4@gmail.com>
2020-03-22 20:28:42 +0300

    mkdir and mv

---
+++ third-week-homework/homework3.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+
+
+// Bu odevde global veya statik degisken tanimlanamaz!!!
+
+/* Ozyinelemeli olarak zip zip kurbaganin kac farkli yoldan gittigini bulan fonksiyondur.
+ * Bu fonksiyon icinde for ve while kullanamazsiniz ve en az bir kez recursiveFunction
+ * fonksiyonu cagri yapilmalidir.
+ */
+
+
+long long recursiveFunction(int numberOfStones)
+{
+	if (numberOfStones ==0 )	//base case
+		return 1;
+	
+		numberOfStones--;
+	return recursiveFunction(numberOfStones) + numberOfStones;
+}
+
+
+/* Yinelemeli olarak zip zip kurbaganin kac farkli yoldan gittigini bulan fonksiyondur.
+ * Bu fonksiyon icinde for ve while dongusu kullanilmalidir.
+ * Bu fonksiyon icinde baska bir fonksiyona (kendisi dahil) cagri yapilamaz.
+ */
+
+long long iterativeFunction(int numberOfStones)
+{
+	long long differentWays = 0;
+	
+	for (int i = 0; i < numberOfStones; i++)
+		differentWays += i;
+
+	differentWays++;
+
+	return differentWays;
+}
+
+
+
+
+int main()
+{
+	const int MAX_NUMBER_OF_STONES{ 72 };
+
+	cout << setfill(' ');
+
+	cout.imbue(locale(""));
+
+	for (auto i = 1; i <= MAX_NUMBER_OF_STONES; ++i)
+	{
+
+		{
+			long long tempResult = { 0 };
+
+			auto start = std::chrono::steady_clock::now();
+			tempResult = iterativeFunction(i);
+			auto stop = std::chrono::steady_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+
+
+			cout << "iterative" << setw(4) << i << " -> " << tempResult << " ( " << duration << "us )\n";
+		}
+
+		{
+			long long tempResult = { 0 };
+
+			auto start = std::chrono::steady_clock::now();
+			tempResult = recursiveFunction(i);
+			auto stop = std::chrono::steady_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+
+			cout << "recursive" << setw(4) << i << " -> " << tempResult << " ( " << duration << "us )\n\n";
+		}
+
+
+	}
+
+	return 0;
+}
